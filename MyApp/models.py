99098f4d9a02929492240c74571dbdbb5bfd95b5
RonitNayak25 <ronitnayak25@gmail.com>
2020-07-17 00:07:45 +0530

    Rendered Data

--- MyApp/models.py
+++ MyApp/models.py
@@ -39,17 +39,17 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
-    # @property
-    # def get_cart_total(self):
-    #     orderitems = self.orderitem_set.all()
-    #     total = sum([item.get_total for item in orderitems])
-    #     return total
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
 
-    # @property
-    # def get_cart_items(self):
-    #     orderitems = self.orderitem_set.all()
-    #     total = sum([item.quantity for item in orderitems])
-    #     return total
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 
 class OrderItem(models.Model):
@@ -63,8 +63,7 @@ class OrderItem(models.Model):
     def __str__(self):
         return self.product.name
 
-
-#     @property
-#     def get_total(self):
-#         total = self.product.price * self.quantity
-#         return total
+    @property
+    def get_total(self):
+        total = float(self.product.price) * int(self.quantity)
+        return total
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
+from .models import *
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {"products": products}
     return render(request, "MyApp/store.html", context)
 
 
 def cart(request):
-    context = {}
+    items = []
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+    context = {"items": items, "order": order}
     return render(request, "MyApp/cart.html", context)
 
 
 def checkout(request):
-    context = {}
+    items = []
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+    context = {"items": items, "order": order}
     return render(request, "MyApp/checkout.html", context)
 
 
